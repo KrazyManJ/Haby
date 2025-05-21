609050b230d99ce7aec32449471ea5582f6d1bc6
xkorcak <xkorcak@mendelu.cz>
2025-05-21 10:25:34 +0200

    Initialized project

---
+++ Haby/HabyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabyApp.swift
+//  Haby
+//
+//  Created by Jaroslav Korčák on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct HabyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
