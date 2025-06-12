5e7262845281fdf632e150f6a504fee33848102e
katapl <plesovakaterina@gmail.com>
2025-06-12 12:44:12 +0200

    updated habit frequency, added habit management view state

--- Haby/Model/HabitFrequency.swift
+++ Haby/Model/HabitFrequency.swift
@@ -1,5 +1,11 @@
 
-enum HabitFrequency {
-    case Daily
-    case Weekly
+enum HabitFrequency: Int16, CaseIterable, Identifiable {
+    var id: Self { self }
+    
+    case Daily = 1
+    case Weekly = 2
+    
+    var name: String {
+            String(describing: self)
+    }
 }
