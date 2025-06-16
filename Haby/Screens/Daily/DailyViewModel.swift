26eff5454c8b6063443e6b70a970fed5fcc80ba2
KrazyManJ <KrazyManJ@gmail.com>
2025-06-16 09:13:35 +0200

    Cleared redundant code from daily view

--- Haby/Screens/Daily/DailyViewModel.swift
+++ Haby/Screens/Daily/DailyViewModel.swift
@@ -5,9 +5,4 @@ import SwiftUI
 class DailyViewModel: ObservableObject {
     var state: DailyViewState = DailyViewState()
     var dataManaging: Injected<DataManaging> = .init()
-    
-    func fetchHabits() {
-        let result: [HabitDefinitionEntity] = dataManaging.wrappedValue.fetch()
-        state.habits = result.map { v in v.toModel() }
-    }
 }
--- Haby/Screens/Daily/DailyViewState.swift
+++ Haby/Screens/Daily/DailyViewState.swift
@@ -2,5 +2,4 @@ import Observation
 
 @Observable
 final class DailyViewState {
-    var habits: [HabitDefinition] = []
 }
