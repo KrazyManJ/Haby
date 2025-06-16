8bdc5b216267a0f766ad2e5c01f897fb73fecd20
Kateřina Plešová <xplesova@node.mendelu.cz>
2025-06-16 15:36:35 +0200

    habit can now be removed, started work on theme

--- Haby/Screens/HabitManagement/HabitManagementViewModel.swift
+++ Haby/Screens/HabitManagement/HabitManagementViewModel.swift
@@ -6,8 +6,24 @@ class HabitManagementViewModel: ObservableObject {
     var state: HabitManagementViewState = HabitManagementViewState()
     var dataManaging: Injected<DataManaging> = .init()
     
+    private var dataManager: DataManaging
+    
+    init() {
+        dataManager = DIContainer.shared.resolve()
+    }
+    
     func fetchHabits() {
         let result: [HabitDefinitionEntity] = dataManaging.wrappedValue.fetch()
         state.habits = result.map { v in v.toModel() }
     }
+    
+    func removeHabit(habit: HabitDefinition) {
+        if let entity: HabitDefinitionEntity = dataManager.fetchOneById(id: habit.id) {
+            dataManager.delete(entity: entity)
+            state.habits.removeAll { $0.id == habit.id }
+        } else {
+            print("Entity not found for id \(habit.id)")
+        }
+    }
+    
 }
