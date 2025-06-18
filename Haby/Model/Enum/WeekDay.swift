281b91d2a0653d256fddaa65777ed1896854e3d7
Kateřina Plešová <xplesova@evo08.mendelu.cz>
2025-06-18 14:35:45 +0200

    fix: added weekday enum

--- Haby/Model/Enum/WeekDay.swift
+++ Haby/Model/Enum/WeekDay.swift
@@ -1,8 +1,18 @@
-//
-//  WeekDay.swift
-//  Haby
-//
-//  Created by Kateřina Plešová on 18.06.2025.
-//
 
 import Foundation
+
+enum WeekDay: Int16, CaseIterable, Identifiable {
+    var id: Self { self }
+    
+    case Monday = 1
+    case Tuesday = 2
+    case Wednesday = 3
+    case Thursday = 4
+    case Friday = 5
+    case Saturday = 6
+    case Sunday = 7
+    
+    var name: String {
+        String(describing: self)
+    }
+}
