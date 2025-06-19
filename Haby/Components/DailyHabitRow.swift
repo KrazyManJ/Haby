445a6cd106eec88e1397ac458126d27a833c4fd3
KrazyManJ <KrazyManJ@gmail.com>
2025-06-19 14:28:45 +0200

    Updated timed daily habits view look and checking logic

--- Haby/Components/DailyHabitRow.swift
+++ Haby/Components/DailyHabitRow.swift
@@ -4,21 +4,46 @@ struct DailyHabitRow: View {
     @Binding var viewModel: DailyViewModel
     @State private var showConfirmation = false
     var habit: HabitDefinition
+    private var isCheckedBinding: Binding<Bool> {
+        Binding<Bool>(
+            get: {
+                viewModel.state.habitRecords.contains(where: { $0.habitDefinition.id == habit.id })
+            },
+            set: { value in
+                if value {
+                    viewModel.checkHabit(habit: habit)
+                } else {
+                    showConfirmation = true
+                }
+            }
+        )
+    }
     
-    var expiredTick: Bool {
-            guard let record = viewModel.state.habitRecords.first(where: { $0.habitDefinition.id == habit.id }),
-                  let targetTimestamp = habit.targetTimestamp,
-                  let timestamp = record.timestamp else {
+    var isChecked: Bool {
+        isCheckedBinding.wrappedValue
+    }
+    
+    var isValid: Bool {
+        if isCheckedBinding.wrappedValue {
+            guard let record = viewModel.state.habitRecords.first(where: { $0.habitDefinition.id == habit.id }) else {
                 return false
             }
-            return timestamp <= targetTimestamp
+            return record.isCompletedOnTime
         }
+        else {
+            var timestamp = Date().hourAndMinutesToMinutesTimestamp
+            if habit.frequency == .Weekly {
+                timestamp += WeekDay.getTodayWeekDay().toTimestamp
+            }
+            return habit.canBeCheckedInTimestamp(timestamp: timestamp)
+        }
+    }
     
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
             HStack(alignment:.bottom) {
                 Rectangle()
-                    .fill(Color.Primary)
+                    .fill(isChecked || !isValid ? Color.backgroundDisabled : Color.Primary)
                     .frame(width: 8, height: 40)
                     .padding([.leading],48)
                 Text(String(format: "%02d:%02d", habit.targetTimestamp! / 60 % 24, habit.targetTimestamp! % 60))
@@ -26,40 +51,36 @@ struct DailyHabitRow: View {
                     .font(.system(.footnote))
             }
             HStack{
-                Image(systemName: habit.icon)
+                let img = Image(systemName: habit.icon)
+                if (isChecked && isValid) {
+                    img.foregroundStyle(Color.textDisabled)
+                }
+                else {
+                    img
+                }
+                
                 Text(habit.name)
                     .foregroundStyle(Color.TextLight)
                     .bold()
-                if let record = viewModel.state.habitRecords.first(where: { $0.habitDefinition.id == habit.id }) {
-                    Text("\(expiredTick)")
-                }
                 Spacer()
-                CheckBox(isOn: Binding(
-                    get: {
-                        viewModel.state.habitRecords.first { $0.habitDefinition.id == habit.id} != nil
-                    },
-                    set: { value in
-                        if !value { showConfirmation = true }
-                        else { viewModel.checkHabit(habit: habit) }
-                    }
-                ))
+                CheckBox(isOn: isCheckedBinding,isInvalid: !isValid)
             }
             .padding(.horizontal, 16)
             .padding(.vertical, 8)
             .background(
                 RoundedRectangle(cornerRadius: 16, style: .continuous)
-                    .fill(Color.Primary)
+                    .fill(isChecked || !isValid ? Color.backgroundDisabled : Color.Primary)
                     .shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 3)
             )
-            .foregroundStyle(Color.Secondary)
+            .foregroundStyle(
+                isValid ? Color.Secondary : (habit.type == .OnTime && !isChecked ? Color.textDisabled : .red )
+            )
         }
         .confirmationDialog("Are you sure you want to uncheck this habit? This action loses your current stage of habit.", isPresented: $showConfirmation, titleVisibility: .visible) {
             Button("Uncheck", role: .destructive) {
                 viewModel.checkHabit(habit: habit)
             }
-            Button("Cancel", role: .cancel) {
-                
-            }
+            Button("Cancel", role: .cancel) {}
         }
     }
 }
